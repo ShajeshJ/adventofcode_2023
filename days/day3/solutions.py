359542b488d85fb0f396bbd8a7ac5e3b8579ed92
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-12-03 23:33:27 -0500

    refactor: reduce code footprint to avoid excessive functions

--- days/day3/solutions.py
+++ days/day3/solutions.py
@@ -52,80 +52,38 @@ def part_1() -> str:
     return str(total)
 
 
-def is_gear(schematic: list[str], i: int, j: int) -> bool:
-    """Returns True if the given position is a gear asterisk"""
-    if schematic[i][j] != "*":
-        return False
-
-    part_num_count = 0
-
-    # Possible part count configurations above/below:
-    # `xNx` or `N$N` where x is anything, N is a digit, and $ is any non-digit
-    # these would result in 1 or 2 part numbers respectively
-    if schematic[i-1][j].isdigit():
-        part_num_count += 1
-    elif schematic[i-1][j-1].isdigit() and schematic[i-1][j+1].isdigit():
-        part_num_count += 2
-
-    if schematic[i+1][j].isdigit():
-        part_num_count += 1
-    elif schematic[i+1][j-1].isdigit() and schematic[i+1][j+1].isdigit():
-        part_num_count += 2
-
-    # Possible part count configurations left/right:
-    if schematic[i][j-1].isdigit():
-        part_num_count += 1
-    if schematic[i][j+1].isdigit():
-        part_num_count += 1
-
-    return part_num_count == 2
-
-
-@dataclass
-class GearMapper:
-    schematic: list[str]
-    gears: dict[tuple[int, int], list[int]] = field(default_factory=dict)
-
-
-    def find_all_gears(self) -> dict[tuple[int, int], list[int]]:
-        num_builder = ""
-
-        for i, line in enumerate(self.schematic):
-            for j, c in enumerate(line):
-                if c.isdigit():
-                    num_builder += c
-                    continue
-
-                if not num_builder:
-                    continue
-
-                # update asterisk to the left of the number
-                if self.schematic[i][j-len(num_builder)-1] == "*":
-                    self.gears.setdefault((i, j-len(num_builder)-1), []).append(int(num_builder))
-                if self.schematic[i][j] == "*":
-                    self.gears.setdefault((i, j), []).append(int(num_builder))
-
-                # update asterisks above/below the number
-                for col in range(j-len(num_builder)-1, j+1):
-                    if self.schematic[i-1][col] == "*":
-                        self.gears.setdefault((i-1, col), []).append(int(num_builder))
-                    if self.schematic[i+1][col] == "*":
-                        self.gears.setdefault((i+1, col), []).append(int(num_builder))
-
-                num_builder = ""
-
-        for pos in list(self.gears.keys()):
-            if len(self.gears[pos]) != 2:
-                del self.gears[pos]
-
-        return self.gears
-
-
 def part_2() -> str:
     schematic = add_schematic_padding(read_problem_input())
+    num_builder = ""
+    gears: dict[tuple[int, int], list[int]] = {}
 
-    gearMapper = GearMapper(schematic)
-    gears = gearMapper.find_all_gears()
+    for i, line in enumerate(schematic):
+        for j, c in enumerate(line):
+            if c.isdigit():
+                num_builder += c
+                continue
+
+            if not num_builder:
+                continue
+
+            # update asterisk to the left of the number
+            if schematic[i][j-len(num_builder)-1] == "*":
+                gears.setdefault((i, j-len(num_builder)-1), []).append(int(num_builder))
+            if schematic[i][j] == "*":
+                gears.setdefault((i, j), []).append(int(num_builder))
+
+            # update asterisks above/below the number
+            for col in range(j-len(num_builder)-1, j+1):
+                if schematic[i-1][col] == "*":
+                    gears.setdefault((i-1, col), []).append(int(num_builder))
+                if schematic[i+1][col] == "*":
+                    gears.setdefault((i+1, col), []).append(int(num_builder))
+
+            num_builder = ""
+
+    for pos in list(gears.keys()):
+        if len(gears[pos]) != 2:
+            del gears[pos]
 
     total = sum(part_nums[0] * part_nums[1] for part_nums in gears.values())
     return str(total)

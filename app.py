5630e83bc880ced6312ce084493456775dcf1fcb
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-12-05 00:22:58 -0500

    chore: add skeleton for day 5 solution

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ import days.day1.solutions as day1
 import days.day2.solutions as day2
 import days.day3.solutions as day3
 import days.day4.solutions as day4
+import days.day5.solutions as day5
 
 ALL_DAYS: dict[int, Solutions] = {
     1: day1,
     2: day2,
     3: day3,
     4: day4,
+    5: day5,
 }
 
 def run_solution(solution: Solutions, part_num: t.Literal[1, 2]) -> None:
---
+++ days/day5/solutions.py
@@ -0,0 +1,5 @@
+def part_1():
+    raise NotImplementedError()
+
+def part_2():
+    raise NotImplementedError()

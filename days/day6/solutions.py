8cb7e873a3e5d43fe4647b704baf7f84f8870909
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-12-06 01:27:12 -0500

    docs: add additional implementation note for part 2

--- days/day6/solutions.py
+++ days/day6/solutions.py
@@ -1,6 +1,7 @@
 # TLDR: literally just implement the formula in code
 # lowerbound > (-t + sqrt(t^2-4*-1*-d)) / 2(-1)
 # upperbound < (-t - sqrt(t^2-4*-1*-d)) / 2(-1)
+# (note: for part 2, you'll probably have to deal with int overflow)
 
 def part_1() -> str:
     raise NotImplementedError()

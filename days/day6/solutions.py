cba97f09b9bce71c66990868559bb560684ef65a
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-12-06 01:26:22 -0500

    solve: pseudo solve day 6 problems with basic math formula

--- days/day6/solutions.py
+++ days/day6/solutions.py
@@ -1,3 +1,7 @@
+# TLDR: literally just implement the formula in code
+# lowerbound > (-t + sqrt(t^2-4*-1*-d)) / 2(-1)
+# upperbound < (-t - sqrt(t^2-4*-1*-d)) / 2(-1)
+
 def part_1() -> str:
     raise NotImplementedError()
 

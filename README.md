d9db6d503955216c0398f8a21b7f0222042e5d3c
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-08-07 04:35:48 -0400

    solve: implement solution for day 8 part 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Advent of Code 2023
 ![Python](https://img.shields.io/badge/python-3.12-blue.svg)
+![](https://img.shields.io/badge/stars%20⭐-16-yellow)
+![](https://img.shields.io/badge/days%20completed-8-red)
 
 My solutions for https://adventofcode.com/2023
--- days/day8/solutions.py
+++ days/day8/solutions.py
@@ -1,5 +1,7 @@
-
+import itertools
+import math
 from dataclasses import dataclass
+from functools import cached_property
 
 from utils import read_problem_input
 
@@ -8,6 +10,11 @@ from utils import read_problem_input
 class Node:
     name: str
     children: tuple[str, str]
+
+    @cached_property
+    def last_char(self) -> str:
+        return self.name[-1]
+
 
 def parse_node_lookup(lines: list[str]) -> dict[str, Node]:
     lookup: dict[str, Node] = {}
@@ -19,6 +26,7 @@ def parse_node_lookup(lines: list[str]) -> dict[str, Node]:
         lookup[name] = Node(name, (left, right))
 
     return lookup
+
 
 def part_1() -> str:
     lines = read_problem_input()
@@ -35,5 +43,38 @@ def part_1() -> str:
             if cur_node.name == "ZZZ":
                 return str(steps)
 
+
+def nav_path_simultaneously(
+    node_table: dict[str, Node], instructions: list[int]
+) -> set[int]:
+    """Simultaneously traverses all paths from **A to **Z and returns the lengths of each path."""
+
+    path_lens: set[int] = set()
+    current_nodes: set[str] = {
+        node.name for node in node_table.values() if node.last_char == "A"
+    }
+
+    for dir, step in zip(itertools.cycle(instructions), itertools.count()):
+        next_nodes: set[str] = set()
+        for node_name in current_nodes:
+            node = node_table[node_name]
+
+            if node.last_char == "Z":
+                path_lens.add(step)
+                continue
+
+            next_nodes.add(node.children[dir])
+
+        if not next_nodes:
+            return path_lens
+
+        current_nodes = next_nodes
+
+
 def part_2() -> str:
-    raise NotImplementedError()
+    lines = read_problem_input()
+    instructions = [0 if c == "L" else 1 for c in lines.pop(0)]
+    node_table = parse_node_lookup(lines[1:])
+
+    path_lens = nav_path_simultaneously(node_table, instructions)
+    return str(math.lcm(*path_lens))

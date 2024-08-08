c552c31d0e957b74da8e7f42e15d9de4c4645bc4
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-08-08 04:36:35 -0400

    solve: day 11 part 2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2023
 ![Python](https://img.shields.io/badge/python-3.12-blue.svg)
-![](https://img.shields.io/badge/stars%20⭐-21-yellow)
-![](https://img.shields.io/badge/days%20completed-10-red)
+![](https://img.shields.io/badge/stars%20⭐-22-yellow)
+![](https://img.shields.io/badge/days%20completed-11-red)
 
 My solutions for https://adventofcode.com/2023
--- days/day11/solutions.py
+++ days/day11/solutions.py
@@ -1,47 +1,97 @@
+import itertools
 from utils import read_problem_input
 
 type Coord = tuple[int, int]
 
 
-def expand(universe: list[str]) -> list[str]:
-    new_universe: list[str] = [""] * len(universe)
+type UniverseRow = list[tuple[str, int]]
+"""A row in the universe, with consecutive characters compressed into tuples"""
+
+type EmptyUniverseRows = int
+"""A count of consecutive empty rows in the universe"""
+
+
+def get_row_len(row: UniverseRow) -> int:
+    return sum(g[1] for g in row)
+
+
+def get_universe() -> list[UniverseRow]:
+    return [
+        [(c, len(list(g))) for c, g in itertools.groupby(line)]
+        for line in read_problem_input()
+    ]
+
+
+def decompress(universe: list[UniverseRow]) -> list[UniverseRow]:
+    """Decompress UniverseRow into individual characters"""
+    return [[(c, 1) for c, n in row for _ in range(n)] for row in universe]
+
+
+def compress(universe: list[UniverseRow]) -> list[UniverseRow]:
+    """Compress UniverseRow into its most compact form"""
+    return [
+        [
+            (c, sum(n[1] for n in g))
+            for c, g in itertools.groupby(row, key=lambda t: t[0])
+        ]
+        for row in universe
+    ]
+
+
+def expand(
+    universe: list[UniverseRow], factor: int
+) -> list[UniverseRow | EmptyUniverseRows]:
+    universe = decompress(universe)
+    new_universe: list[UniverseRow] = [[] for _ in range(len(universe))]
 
     # Column expansion
-    for x in range(len(universe[0])):
+    for x in range(get_row_len(universe[0])):
         col_chars: set[str] = set()
 
         for y, row in enumerate(universe):
-            col_chars.add(row[x])
-            new_universe[y] += row[x]
+            col_chars.add(row[x][0])
+            new_universe[y].append(row[x])
 
         if col_chars == {"."}:  # Empty column
-            new_universe = [row + "." for row in new_universe]
+            for new_row in new_universe:
+                new_row[-1] = (new_row[-1][0], factor)
+
+    new_universe = compress(new_universe)
 
     # Row expansion
     universe = new_universe
     new_universe = []
-    empty_row = "." * len(universe[0])
+    empty_row: UniverseRow = [(".", get_row_len(universe[0]))]
 
     for row in universe:
-        new_universe.append(row)
         if row == empty_row:
-            new_universe.append(empty_row)
+            new_universe.append(factor)
+        else:
+            new_universe.append(row)
 
     return new_universe
 
 
-def get_galaxies(universe: list[str]) -> list[Coord]:
+def get_galaxies(universe: list[UniverseRow | EmptyUniverseRows]) -> list[Coord]:
     galaxies: list[Coord] = []
+    yskip = 0
+
     for y, row in enumerate(universe):
-        for x, cell in enumerate(row):
-            if cell == "#":
-                galaxies.append((y, x))
+        if isinstance(row, int):
+            yskip += row - 1
+            continue
+
+        x = 0
+        for c, n in row:
+            if c == "#":
+                galaxies.extend((y + yskip, x + i) for i in range(n))
+            x += n
+
     return galaxies
 
 
-def part_1() -> str:
-    universe = read_problem_input()
-    universe = expand(universe)
+def part_1(factor: int = 2) -> str:
+    universe = expand(get_universe(), factor)
     galaxies = get_galaxies(universe)
 
     shortest_distance_sum = 0
@@ -55,4 +105,5 @@ def part_1() -> str:
     return str(shortest_distance_sum)
 
 
-def part_2() -> str: ...
+def part_2() -> str:
+    return part_1(1_000_000)
